969501ffc7b3a6b4b3a9f1bf3631a563106cbd45
Mohamed-Magdey <ragabmohame6@gmail.com>
2024-11-20 18:04:16 +0200

    Prepare basic layout & general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  searchQuery = signal('');
 
+  onSearch(event: string): void {
+    console.log(event);
+    this.searchQuery.set(event);
+  }
+
+  clearSearch(): void {
+
+  }
 }
